b1a17c1c8fafc6395e1a5c7f2a20ea779050f2ba
Noah Blumenthal <noah@hackerhasid.com>
2014-09-08 11:33:15 -0400

    moar

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -3,6 +3,12 @@ include_recipe "deploy::rails-restart"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
+
   template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
     source "secrets.yml.erb"
     mode 0660
@@ -11,12 +17,8 @@ node[:deploy].each do |application, deploy|
     cookbook "secrets_yml"
     variables(:environment_variables => deploy[:environment], :rails_env => deploy[:rails_env])
 
-    execute "restart Rails app #{application}" do
-      cwd deploy[:current_path]
-      command node[:opsworks][:rails_stack][:restart_command]
-      action :nothing
-    end
-
+    notifies :run, "execute[restart Rails app #{application}]"
+    
     only_if do
       File.directory?("#{deploy[:deploy_to]}/shared/config/")
     end
